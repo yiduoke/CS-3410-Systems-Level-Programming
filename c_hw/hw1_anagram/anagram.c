fa661e2ed32c64dabfe90e7b1ca7fa11715a7eab
Yiduo Ke <yiduoke822@jhs185.org>
2020-02-12 19:47:44 -0500

    C assignment anagram done

--- c_hw/hw1_anagram/anagram.c
+++ c_hw/hw1_anagram/anagram.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
@@ -10,10 +11,50 @@ int main()
     scanf("%s", str1);
     printf("Input second string: ");
     scanf("%s", str2);
+    char temp;
 
     // YOUR CODE STARTS HERE
 
     // Check if str2 is an anagram of str1.
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    
+    if (len1 != len2){
+        flag = 0;
+    }
+    else{
+        char strr1[100];
+        strcpy(strr1, str1);
+        char strr2[100];
+        strcpy(strr2, str2);
+
+        // insertion sort for the string
+        for (int i = 0; i < len1 - 1; i++) {
+            for (int j = i+1; j < len1; j++) {
+                if (strr1[i] > strr1[j]) {
+                    temp = strr1[i];
+                    strr1[i] = strr1[j];
+                    strr1[j] = temp;
+                }
+            }
+        }
+        
+        for (int i = 0; i < len1-1; i++) {
+            for (int j = i+1; j < len1; j++) {
+                if (strr2[i] > strr2[j]) {
+                    temp = strr2[i];
+                    strr2[i] = strr2[j];
+                    strr2[j] = temp;
+                }
+            }
+        }
+        if (!strcmp(strr1, strr2)){
+            flag = 0;
+        }
+        else{
+            flag = 1;
+        }
+    }
 
     // YOUR CODE ENDS HERE
 
